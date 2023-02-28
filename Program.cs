a1c3d70bdb376a5154ff84fd2d56e6add1511623
Влад Михайличенко <mykhailichenkovlad@gmail.com>
2023-02-28 23:42:36 +0200

    first task

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+#region 1
+
+string firstStr = "А роза упала на лапу Азора";
+string secondStr = firstStr.ToLower().Replace(" ", ""); // Удаляем все пробелы и уменьшаем капс
+string reverseStr = new string(secondStr.ToCharArray().Reverse().ToArray()); // Переворачиваем строку
+
+if (secondStr == reverseStr)
+    Console.WriteLine($"Строка \"{firstStr}\" является палиндромом");
+else
+    Console.WriteLine($"Строка \"{firstStr}\" не является палиндромом");
+
+#endregion
